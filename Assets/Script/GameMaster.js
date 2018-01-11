be8e9f07fb38fb52779d865e789ee8613069cf4c
Andrew Friedman <andrewfriedman01@gmail.com>
2018-01-10 22:40:36 -0500

    Added App
    This is the first version of Free Trump SOundboard that GitHub has ever seen!

---
+++ Assets/Script/GameMaster.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+function MainMenu() {
+	Application.LoadLevel("MainMenu");
+}
+
+function MainMenu2() {
+	Application.LoadLevel("MainMenu2");
+}
+
+function MainMenu3() {
+	Application.LoadLevel("MainMenu3");
+}
+
+function MainMenu4() {
+	Application.LoadLevel("MainMenu4");
+}
+
+function New() {
+	Application.OpenURL ("https://play.google.com/store/apps/details?id=com.Anderita.Trumpy2");
+}
---
+++ Assets/Script/Player.js
@@ -0,0 +1,110 @@
+﻿#pragma strict
+var sound1:AudioClip;
+var sound2:AudioClip;
+var sound3:AudioClip;
+var sound4:AudioClip;
+var sound5:AudioClip;
+var sound6:AudioClip;
+var sound7:AudioClip;
+var sound8:AudioClip;
+var sound9:AudioClip;
+var sound10:AudioClip;  //Var name may vary >:)
+var sound11:AudioClip;
+var sound12:AudioClip;
+var sound13:AudioClip;
+var sound14:AudioClip;
+var sound15:AudioClip;
+var sound16:AudioClip;
+var sound17:AudioClip;
+var sound18:AudioClip;
+var sound19:AudioClip;
+var sound20:AudioClip;
+var sound21:AudioClip;
+
+function sounds1()
+{
+	GetComponent.<AudioSource>().PlayOneShot(sound1);
+}
+function sounds2()
+{
+	GetComponent.<AudioSource>().PlayOneShot(sound2);
+}
+function sounds3()
+{
+	GetComponent.<AudioSource>().PlayOneShot(sound3);
+}
+function sounds4()
+{
+	GetComponent.<AudioSource>().PlayOneShot(sound4);
+}
+function sounds5()
+{
+	GetComponent.<AudioSource>().PlayOneShot(sound5);
+}
+function sounds6()
+{
+	GetComponent.<AudioSource>().PlayOneShot(sound6);
+}
+function sounds7()
+{
+	GetComponent.<AudioSource>().PlayOneShot(sound7);
+}
+function sounds8()
+{
+	GetComponent.<AudioSource>().PlayOneShot(sound8);
+}
+function sounds9()
+{
+	GetComponent.<AudioSource>().PlayOneShot(sound9);
+}
+function sounds10()
+{
+	GetComponent.<AudioSource>().PlayOneShot(sound10);
+}
+function sounds11()
+{
+	GetComponent.<AudioSource>().PlayOneShot(sound11);
+}
+function sounds12()
+{
+	GetComponent.<AudioSource>().PlayOneShot(sound12);
+}
+function sounds13()
+{
+	GetComponent.<AudioSource>().PlayOneShot(sound13);
+}
+function sounds14()
+{
+	GetComponent.<AudioSource>().PlayOneShot(sound14);
+}
+function sounds15()
+{
+	GetComponent.<AudioSource>().PlayOneShot(sound15);
+}
+function sounds16()
+{
+	GetComponent.<AudioSource>().PlayOneShot(sound16);
+}
+function sounds17()
+{
+	GetComponent.<AudioSource>().PlayOneShot(sound17);
+}
+function sounds18()
+{
+	GetComponent.<AudioSource>().PlayOneShot(sound18);
+}
+function sounds19()
+{
+	GetComponent.<AudioSource>().PlayOneShot(sound19);
+}
+function sounds20()
+{
+	GetComponent.<AudioSource>().PlayOneShot(sound20);
+}
+function sounds21()
+{
+	GetComponent.<AudioSource>().PlayOneShot(sound21);
+}
+function Mute() {
+	GetComponent.<AudioSource>().Stop();
+}
